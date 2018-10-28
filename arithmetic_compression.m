2f06c1c8839a83b1ffa32fe3717539c5bcb6f861
Jose Manuel Rodriguez Calvo <34111274+jmrCalvo@users.noreply.github.com>
2018-10-29 00:01:51 +0100

    Update arithmetic_compression.m

--- arithmetic_compression.m
+++ arithmetic_compression.m
@@ -37,7 +37,7 @@ fprintf('¿Coinciden original y comprimido 1(S) 0 (N)?, %d\n',...
 %% Paso 7
 
 close all; clear all;
-seqob=[1 1];Arithme
+seqob=[1 1];
 counts=[1 1];
 code=arithenco(seqob,counts);
 fprintf('Longitud de la secuencia codificada %d\n',...
@@ -65,4 +65,81 @@ minimo=min(seq(:));
 end
 histo=histc(seq,[1 2]);
 bar([1 2],histo);
+
+%% Paso 12
+
+code=arithenco(seq,histo);
+fprintf('Longitud de la secuencia codificada %d\n',numel(code));
+indseqdec=arithdeco(code,histo,numel(seq));
+fprintf('¿Coinciden original y comprimido 1(S) 0 (N)?, %d\n',...
+    isequal(seq,indseqdec))
+
+%% Paso 13
+
+clear all;close all; clc;
+sol=[]
+medias=[]
+entropias=[]
+for j=1:6
+    sol_partial=[]
+   for i=1:5
+    maximo= 0.0;
+    minimo=0.0;
+    %%rng(0);
+    while maximo==minimo
+    seq=randsrc(1,10^j,[1 2; 0.1 0.9]);
+    maximo=max(seq(:));
+    minimo=min(seq(:));
+    end
+    histo=histc(seq,[1 2]);
+    %%bar([1 2],histo);
+    code=arithenco(seq,histo);
+    %%fprintf('Longitud de la secuencia codificada %d\n',numel(code));
+    sol_partial=[sol_partial,numel(code)];
+    entropias=[entropias,entropiaJMRC(histo)]
+   end
+   sol=[sol,sol_partial]
+   media=median(sol_partial)
+   medias=[medias,media]
+end
+
+subplot(1,3,1);plot(entropias);
+subplot(1,3,2);bar([1:30],sol)
+subplot(1,3,3);bar([1:6],medias);
+
+
+%% Paso 14 
+clear all;close all; clc;
+
+fichero='texto10.txt'
+fid=fopen(fichero, 'r')
+bits=fread(fid,inf,'*uint8');
+fclose(fid)
+
+seq=reshape(bits,1,length(bits));
+
+uv = unique(seq);
+counts  = histc(seq,uv);
+
+aux=ones(1,length(seq));
+aux=aux.*47;
+seq=seq-uint8(aux)
+
+code=arithenco(seq,counts);
+
+indseqdec=arithdeco(code,counts,length(seq));
+indseqdec=indseqdec+uint8(aux)
+seqdecf=alf(indseqdec);
+fprintf('¿Coinciden original y comprimido 1(S) 0 (N)?, %d\n',...
+    isequal(seq,seqdecf))
+
+end
+
+function [resultado]= entropiaJMRC(histograma)
+    
+suma=sum(histograma);
+prob= histograma./suma;
+prob(find(prob==0.0))=[];
+resultado=-sum(prob.*log2(prob));
+
 end
