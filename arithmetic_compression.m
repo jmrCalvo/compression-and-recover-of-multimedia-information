89ac5e9e9d1eb978512d4eb497d7c1f384c5e27a
Jose Manuel Rodriguez Calvo <34111274+jmrCalvo@users.noreply.github.com>
2018-11-03 18:10:45 +0100

    Update arithmetic_compression.m

--- arithmetic_compression.m
+++ arithmetic_compression.m
@@ -133,6 +133,8 @@ indseqdec=arithdeco(code,counts,length(seq));
 seqdecf=uv(indseqdec);
 fprintf('¿Coinciden original y comprimido 1(S) 0 (N)?, %d\n',...
     isequal(seq_original,seqdecf))
+fprintf('Longitud de Bit por simbolo %d\n',numel(code)/numel(seq_original));
+
 
 %% texto100
 clear all;close all; clc;
@@ -159,6 +161,7 @@ indseqdec=arithdeco(code,counts,length(seq));
 seqdecf=uv(indseqdec);
 fprintf('¿Coinciden original y comprimido 1(S) 0 (N)?, %d\n',...
     isequal(seq_original,seqdecf))
+fprintf('Longitud de Bit por simbolo %d\n',numel(code)/numel(seq_original));
 %% texto1000
 clear all;close all; clc;
 
@@ -184,6 +187,7 @@ indseqdec=arithdeco(code,counts,length(seq));
 seqdecf=uv(indseqdec);
 fprintf('¿Coinciden original y comprimido 1(S) 0 (N)?, %d\n',...
     isequal(seq_original,seqdecf))
+fprintf('Longitud de Bit por simbolo %d\n',numel(code)/numel(seq_original));
 
 %% texto10000
 clear all;close all; clc;
@@ -211,6 +215,8 @@ seqdecf=uv(indseqdec);
 fprintf('¿Coinciden original y comprimido 1(S) 0 (N)?, %d\n',...
     isequal(seq_original,seqdecf))
 
+fprintf('Longitud de Bit por simbolo %d\n',numel(code)/numel(seq_original));
+
 %% texto100000
 clear all;close all; clc;
 
@@ -236,8 +242,155 @@ indseqdec=arithdeco(code,counts,length(seq));
 seqdecf=uv(indseqdec);
 fprintf('¿Coinciden original y comprimido 1(S) 0 (N)?, %d\n',...
     isequal(seq_original,seqdecf))
+fprintf('Longitud de Bit por simbolo %d\n',numel(code)/numel(seq_original));
+
+%% texto1000000
+clear all;close all; clc;
+
+fichero='texto1000000.txt'
+fid=fopen(fichero, 'r')
+bits=fread(fid,inf,'*uint8');
+fclose(fid)
+
+
+seq_original=reshape(bits,1,length(bits));
+
+uv = unique(seq_original);
+counts  = histc(seq_original,uv);
+
+aux=ones(1,length(seq_original));
+aux=aux.*47;
+seq=seq_original-uint8(aux);
+
+code=arithenco(seq,counts);
+
+indseqdec=arithdeco(code,counts,length(seq));
+%definir alfa
+seqdecf=uv(indseqdec);
+fprintf('¿Coinciden original y comprimido 1(S) 0 (N)?, %d\n',...
+    isequal(seq_original,seqdecf))
+fprintf('Longitud de Bit por simbolo %d\n',numel(code)/numel(seq_original));
+
+
+%% Paso 15
+
+%% cinco semanas en globo
+clear all;close all; clc;
+
+fichero='Cinco Semanas en globo - Julio Verne.txt'
+fid=fopen(fichero, 'r')
+bits=fread(fid,inf,'*uint8');
+fclose(fid)
+
+
+seq_original=reshape(bits,1,length(bits));
+
+uv = unique(seq_original);
+[counts,seq_index]  = histc(seq_original,uv);
+
+code=arithenco(seq_index,counts);
+
+indseqdec=arithdeco(code,counts,length(seq_index));
+
+bar([1:numel(counts)],counts);
+seqdecf=uv(indseqdec);
+fprintf('¿Coinciden original y comprimido 1(S) 0 (N)?, %d\n',...
+    isequal(seq_original,seqdecf))
+fprintf('tamaño original %d\n',numel(seq_original));
+fprintf('Longitud de Bit por simbolo %d\n',numel(code)/numel(seq_original));
+fprintf('factor de compresion %d\n',numel(seq_original)/numel(code));
+HuffmmanJMRC(fichero)
+%% constitucion española
+clear all;close all; clc;
+
+fichero='constitucion española.txt'
+fid=fopen(fichero, 'r')
+bits=fread(fid,inf,'*uint8');
+fclose(fid)
+
+
+seq_original=reshape(bits,1,length(bits));
+
+uv = unique(seq_original);
+[counts,seq_index]  = histc(seq_original,uv);
+
+code=arithenco(seq_index,counts);
+
+indseqdec=arithdeco(code,counts,length(seq_index));
+
+bar([1:numel(counts)],counts);
+seqdecf=uv(indseqdec);
+fprintf('¿Coinciden original y comprimido 1(S) 0 (N)?, %d\n',...
+    isequal(seq_original,seqdecf))
+
+fprintf('tamaño original %d\n',numel(seq_original));
+fprintf('Longitud de Bit por simbolo %d\n',numel(code)/numel(seq_original));
+fprintf('factor de compresion %d\n',numel(seq_original)/numel(code));
+HuffmmanJMRC(fichero)
+%% FUNDACION E IMPERIO - Isaac Asimov
+clear all;close all; clc;
+
+fichero='FUNDACION E IMPERIO - Isaac Asimov.txt'
+fid=fopen(fichero, 'r')
+bits=fread(fid,inf,'*uint8');
+fclose(fid)
+
+
+seq_original=reshape(bits,1,length(bits));
+
+uv = unique(seq_original);
+[counts,seq_index]  = histc(seq_original,uv);
+
+code=arithenco(seq_index,counts);
+
+indseqdec=arithdeco(code,counts,length(seq_index));
+
+bar([1:numel(counts)],counts);
+seqdecf=uv(indseqdec);
+fprintf('¿Coinciden original y comprimido 1(S) 0 (N)?, %d\n',...
+    isequal(seq_original,seqdecf))
+
+fprintf('tamaño original %d\n',numel(seq_original));
+fprintf('Longitud de Bit por simbolo %d\n',numel(code)/numel(seq_original));
+fprintf('factor de compresion %d\n',numel(seq_original)/numel(code));
+HuffmmanJMRC(fichero)
+
+
+%% Paso 16
+
+
 end
 
+function [resultado]= HuffmmanJMRC(archivo)
+    fichero= archivo
+    fid=fopen(fichero,'r')
+    seq=fread(fid,'*uint8');
+    fclose(fid);
+    seq=reshape(seq,1,length(seq)); %leidos datos en seq vector fila
+    fprintf('Huffman: Tamaño %d\n',numel(seq))
+    letras=[0:255];
+    histo=histc(seq,letras); %calculamos histograma e indices
+    posiciones=find(histo>0.0);
+    tam=numel(seq);
+    prob_letras_usadas=histo(posiciones)./tam;
+    letras_usadas=letras(posiciones);
+
+    [dict,avglen] = huffmandict(letras_usadas,prob_letras_usadas) ; %construimos el diccionario
+    seq_codificada = huffmanenco(seq,dict); %codificamos la señal
+
+    tamagno_comprimido=(length(letras_usadas)+1)*1+ ...
+    (length(letras_usadas)+1)*8+ ...
+    ceil(length(seq_codificada)/8);
+    fprintf('Huffman: Bits por dato  %d\n',tamagno_comprimido/numel(seq))
+    
+    factorcompresion=numel(seq)/tamagno_comprimido;
+    fprintf('Huffman: factor de compresion %d\n',factorcompresion)
+    
+    deco=huffmandeco(seq_codificada,dict);
+    resultado=isequal(seq,uint8(deco))
+    
+    
+end
 function [resultado]= entropiaJMRC(histograma)
     
 suma=sum(histograma);
