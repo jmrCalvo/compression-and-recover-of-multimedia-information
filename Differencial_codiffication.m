c80f35eff19dca411e9dc46e42c6fb6b3f3f09c6
Jose Manuel Rodriguez Calvo <34111274+jmrCalvo@users.noreply.github.com>
2018-11-30 19:42:26 +0100

    Update Differencial_codiffication.m

--- Differencial_codiffication.m
+++ Differencial_codiffication.m
@@ -1,103 +1,159 @@
-%% Practica07RodriguezJoseManuel
+%% Practica05RodriguezJoseManuel
 
-function pratica7JMRC()
-%% Paso 1
-clc;clear all;close all;
+% Tiempo: Incluye aquí el tiempo dedicado a realizar el guion
 
-[y,fs]=audioread('lazyrn.16bits.wav');
+function pratica5JMRC()
 
 %% Paso 2
-
-plot(y);
-sound(y,fs);
-
-%% Paso 3
-
-predictor=dpcmopt(y,1)
-
-%% Paso 4
-
-[predictor,codebook,partition]=dpcmopt(y,1,4);
-
-%% Paso 5
-
-indx=dpcmenco(y,codebook,partition,predictor);
-
-%% Paso 6
-
-y2= dpcmdeco(indx,codebook,predictor);
-sound(y2,fs);
-
-%% Paso 7
-
-plot(y(1:150),'b')
-hold on; 
-plot(y2(1:150),'r'); 
-error = y(:)-y2(:);
-plot(error(1:150),'g'); 
-legend('original', 'reconstruida', 'error')
-sound(error,fs)
-
-%% Paso 10
-uv = unique(y);
-histograma=histc(y,uv);
-plot(uv,histograma);
-entropie=entropiaJMRC(histograma);
-
-y_trans=reshape(y,1,length(y));
-error=sum((y_trans-y2).^2)/length(y);
-
-
-%% Paso 13
-
 close all; clear all;
-x=[0:999]/1000;
-y=sin(10*pi*x);
-plot(x,y)
+img=imread("bird.pgm");
 
+s=dir("bird.pgm");
+tamOrig=s.bytes
 
-[predictor,codebook,partition]=dpcmopt(y,1,9);
-indx=dpcmenco(y,codebook,partition,predictor);
-y2= dpcmdeco(indx,codebook,predictor);
-
-uv = unique(y);
-histograma=histc(y,uv);
-plot(uv,histograma);
-
-entropie=entropiaJMRC(histograma);
-error=sum((y-y2).^2)/length(y);
-%% Paso 16
-
-close all; clear all;
-x=[0:999]/1000;
-y=cos(10*pi*x);
-plot(x,y);
-
-%% Paso 18 
-y_min_1=y(1:length(y)-1);
-
-y_norm=y(2:length(y));
-N=length(y_norm);
-
-s_min_1=sum(y_min_1);
-s=sum(y_norm);
-m1=[s_min_1*s,s];
-
-m2=inv([-s_min_1^2,-s_min_1;-N*s_min_1,-N])
-
-sol=m1*m2;
-a=sol(1);b=sol(2);
-
+for i = 5:5:100 
+    filename=sprintf('result/bird%i.jpeg',i)
+    imwrite(img, filename, 'Quality', i);
+    ends
 
 end
+%% Paso 4 int16
+close all;
 
+imgOriginal=imread("bird.pgm");
+[Mo,No]=size(imgOriginal);
+seqOriginal=int16(reshape(imgOriginal,1,Mo*No));
 
-function [resultado]= entropiaJMRC(histograma)
+tasas=[]
+distorsiones=[]
 
-histogramaNZ=histograma;
-histogramaNZ(find(histogramaNZ==0.0))=[];
-suma=sum(histogramaNZ);
-prob= histogramaNZ./suma;
-resultado=-sum(prob.*log2(prob));
+% la tasa es el tamano del fichero original/fichero comprimido => 8/fc
+for i = 5:5:100 
+    filename=sprintf('result/bird%i.jpeg',i);
+    img=imread(filename);
+    [M,N]=size(img);
+    seq=int16(reshape(img,1,M*N));
+            
+    
+    s = dir(filename);
+    tamJPEG=s.bytes;
+    
+    fc=tamOrig/tamJPEG;
+    tasa=8/fc;
+    
+    dif=seqOriginal-seq;
+    dif_cuadratic=dif.^2;
+    
+    error=sum(dif_cuadratic)/(M*N);
+    
+    fprintf('La razon de bird %i es %3.5f y el error es %3.5f\n\n',i,tasa,error)
+    tasas=[tasas,tasa];
+    distorsiones=[distorsiones,error];
+    
+    plot(tasas,distorsiones,'.r');
+end
+%% Paso 6
+close all; clear all;clc;
+
+for i = 2:2:40 
+    filename=sprintf('result/bird%i.jp2',i);
+    imwrite(img,filename,'CompressionRatio',i);
+    
+end
+
+%% Paso 7
+close all; clear all;
+
+imgOriginal=imread("bird.pgm");
+
+s=dir("bird.pgm");
+tamOrig=s.bytes
+
+[Mo,No]=size(imgOriginal);
+seqOriginal=uint16(reshape(imgOriginal,1,Mo*No));
+
+tasas=[]
+distorsiones=[]
+
+for i = 40:-2:2 
+    filename=sprintf('result/bird%i.jp2',i);
+    img=imread(filename);
+    [M,N]=size(img);
+    seq=uint16(reshape(img,1,M*N));
+    s = dir(filename);
+    tamJPG2000=s.bytes;
+    
+    fc=tamOrig/tamJPG2000;
+    tasa=8/fc;
+    
+    dif=seqOriginal-seq;
+    dif_cuadratic=dif.^2;
+    
+    error=sum(dif_cuadratic)/(M*N);
+    
+    fprintf('La razon de bird %i es %3.5f y el error es %3.5f\n\n',i,tasa,error)
+    tasas=[tasas,tasa];
+    distorsiones=[distorsiones,error];
+    
+    plot(tasas,distorsiones,'.r');
+end
+
+%% Paso 10
+close all; clear all;
+
+imgOriginal=imread("bird.pgm");
+s=dir("bird.pgm");
+tamOrig=s.bytes
+
+[Mo,No]=size(imgOriginal);
+seqOriginal=uint8(reshape(imgOriginal,1,Mo*No));
+
+tasasJPEG=[]
+distorsionesJPEG=[]
+tasasJPEG2000=[]
+distorsionesJPEG2000=[]
+
+for i = 5:5:100 
+    filename=sprintf('result/bird%i.jpeg',i);
+    img=imread(filename);
+    [M,N]=size(img);
+    seq=uint8(reshape(img,1,M*N));
+    s = dir(filename);
+    tamJPG2000=s.bytes;
+    
+    fc=tamOrig/tamJPG2000;
+    tasa=8/fc;
+    
+    dif=seqOriginal-seq;
+    dif_cuadratic=dif.^2;
+    
+    error=sum(dif_cuadratic)/(M*N);
+    tasasJPEG=[tasasJPEG,tasa];
+    distorsionesJPEG=[distorsionesJPEG,error];
+    
+    
+end
+for i = 40:-2:2 
+    filename=sprintf('result/bird%i.jp2',i);
+    img=imread(filename);
+    [M,N]=size(img);
+    seq=uint8(reshape(img,1,M*N));
+    s = dir(filename);
+    tamJPG2000=s.bytes;
+    
+    fc=tamOrig/tamJPG2000;
+    tasa=8/fc;
+    
+    dif=seqOriginal-seq;
+    dif_cuadratic=dif.^2;
+    
+    error=sum(dif_cuadratic)/(M*N);
+    tasasJPEG2000=[tasasJPEG2000,tasa];
+    distorsionesJPEG2000=[distorsionesJPEG2000,error];
+    
+    
+end
+
+    plot(tasasJPEG,distorsionesJPEG,tasasJPEG2000,distorsionesJPEG2000);
 
 end
